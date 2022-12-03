29853c96acec85bbf58171f77e194b3f4046872c
Niklas (Krymonota) <krymonota@users.noreply.github.com>
2022-12-03 14:34:15 +0100

    Add `constants.php`

---
+++ constants.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Defines constants for autocompletion in IDEs. This file is not meant to be actively used anywhere!
+ *
+ * @author      Niklas Friedrich Gerstner
+ * @copyright   2022 Krymo Software
+ * @license     Krymo Software - Commercial Products License <https://krymo.software/license-terms/#commercial-products>
+ */
+
+// option constants
+define('PROFILE_GROUPLIST_HIDDEN_GROUPS', '');
