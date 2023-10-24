caa16a3a40eb7f88a30ac2c503612903b5bb5542
Peter Lohse <hanashi@hanashi.eu>
2023-10-24 12:56:05 +0200

    PSR-12 codestyle

--- files/lib/system/event/listener/UserPageGroupListListener.class.php
+++ files/lib/system/event/listener/UserPageGroupListListener.class.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace wcf\system\event\listener;
 
 use wcf\data\user\group\UserGroup;
@@ -14,10 +15,11 @@ use wcf\util\ArrayUtil;
  * @license     Krymo Software - Free Products License <https://krymo.software/license-terms/#free-products>
  * @package     WoltLabSuite\Core\System\Event\Listener
  */
-class UserPageGroupListListener implements IParameterizedEventListener {
+class UserPageGroupListListener implements IParameterizedEventListener
+{
     /**
      * instance of UserPage
-     * @var	UserPage
+     * @var UserPage
      */
     protected $eventObj;
 
@@ -36,62 +38,69 @@ class UserPageGroupListListener implements IParameterizedEventListener {
     /**
      * @inheritDoc
      */
-    public function execute($eventObj, $className, $eventName, array &$parameters) {
+    public function execute($eventObj, $className, $eventName, array &$parameters)
+    {
         $this->eventObj = $eventObj;
-        $this->$eventName();
+        $this->{$eventName}();
     }
 
     /**
      * Handles the assignVariables event.
      */
-    protected function assignVariables() {
+    protected function assignVariables()
+    {
         WCF::getTPL()->assign([
             'canViewUserPageGroupList' => $this->canViewUserPageGroupList,
-            'userGroups' => $this->userGroups
+            'userGroups' => $this->userGroups,
         ]);
     }
 
     /**
      * Handles the readData event.
      */
-    protected function readData() {
+    protected function readData()
+    {
         $user = $this->eventObj->user;
         $this->canViewUserPageGroupList = WCF::getSession()->getPermission('user.profile.canViewUserPageGroupList');
 
         if (!$this->canViewUserPageGroupList) {
             $isOwnProfile = $user->userID === WCF::getUser()->userID;
-            $this->canViewUserPageGroupList = $isOwnProfile && WCF::getSession()->getPermission('user.profile.canViewUserPageGroupListOwnProfile');
+            $this->canViewUserPageGroupList = $isOwnProfile && WCF::getSession()->getPermission(
+                'user.profile.canViewUserPageGroupListOwnProfile'
+            );
         }
 
         if ($this->canViewUserPageGroupList) {
-            $hiddenGroupIDs = ArrayUtil::toIntegerArray(ArrayUtil::trim(explode(',', PROFILE_GROUPLIST_HIDDEN_GROUPS)));
-            $shownGroupIDs = array_diff($user->getGroupIDs(), $hiddenGroupIDs);
+            $hiddenGroupIDs = ArrayUtil::toIntegerArray(
+                ArrayUtil::trim(\explode(',', PROFILE_GROUPLIST_HIDDEN_GROUPS))
+            );
+            $shownGroupIDs = \array_diff($user->getGroupIDs(), $hiddenGroupIDs);
             $userGroups = UserGroup::getGroupsByIDs($shownGroupIDs);
 
-            switch(PROFILE_GROUPLIST_SORT_BY) {
-                case 'priority_desc': {
+            switch (PROFILE_GROUPLIST_SORT_BY) {
+                case 'priority_desc':
                     \uasort($userGroups, static function (UserGroup $groupA, UserGroup $groupB) {
                         return static::compareGroupPriority($groupA, $groupB);
                     });
                     break;
-                }
-                case 'priority_asc': {
+
+                case 'priority_asc':
                     \uasort($userGroups, static function (UserGroup $groupA, UserGroup $groupB) {
                         return static::compareGroupPriority($groupA, $groupB, false);
                     });
                     break;
-                }
-                case 'alphabetical': {
+
+                case 'alphabetical':
                     UserGroup::sortGroups($userGroups);
                     break;
-                }
             }
 
             $this->userGroups = $userGroups;
         }
     }
 
-    private static function compareGroupPriority(UserGroup $groupA, UserGroup $groupB, bool $sortDescending = true): int {
+    private static function compareGroupPriority(UserGroup $groupA, UserGroup $groupB, bool $sortDescending = true): int
+    {
         if ($groupA->priority === $groupB->priority) {
             return 0;
         }
